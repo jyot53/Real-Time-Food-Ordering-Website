018e7a5cebecce2c2cc81ac6d28ee3344cdc03cb
LAPTOP-4FPDI2SI <jyotsoni0053@gmail.com>
2021-06-22 23:57:40 +0530

    addition of cart page , login , register

--- server.js
+++ server.js
@@ -9,14 +9,26 @@ const viewsPath = path.join(__dirname,'/resources/views');
 const staticPath = path.join(__dirname,'/public');
 
 app.use(express.static(staticPath));
-app.get("/",(req,res)=>{
-    res.render("home");    
-});
-
 
 app.use(expressLayout);
 app.set('views',viewsPath);
 app.set('view engine','ejs');
+
+app.get("/",(req,res)=>{
+    res.render("home");    
+});
+
+app.get("/cart",(req,res)=>{
+    res.render("customers/cart");
+});
+app.get("/login",(req,res)=>{
+    res.render("auth/login");
+});
+app.get("/register",(req,res)=>{
+    res.render("auth/register");
+});
+
+
 
 app.listen(port,()=>{
     console.log(`Listening to the port ${port}`);
